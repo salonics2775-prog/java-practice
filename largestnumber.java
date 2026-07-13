46b844982f060a7064f768dcfd9dde6e24c8da0b
Salonilanke-dev <saloni.cs23173@mmcc.edu.in>
2026-07-13 16:06:34 +0530

    Added Largest Number program

---
+++ largestnumber.java
@@ -0,0 +1,13 @@
+public class LargestNumber {
+    public static void main(String[] args) {
+
+        int a = 20;
+        int b = 50;
+
+        if (a > b) {
+            System.out.println(a + " is Largest");
+        } else {
+            System.out.println(b + " is Largest");
+        }
+    }
+}
