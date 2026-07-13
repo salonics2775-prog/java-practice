f8c16f69fa3038117a3cc04cc05c983b9bdd41b4
Salonilanke-dev <saloni.cs23173@mmcc.edu.in>
2026-07-13 16:06:56 +0530

    Added Smallest Number program

---
+++ smallestnumber.java
@@ -0,0 +1,13 @@
+public class SmallestNumber {
+    public static void main(String[] args) {
+
+        int a = 20;
+        int b = 50;
+
+        if (a < b) {
+            System.out.println(a + " is Smallest");
+        } else {
+            System.out.println(b + " is Smallest");
+        }
+    }
+}
