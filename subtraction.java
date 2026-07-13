c1209a7f89957848534b4eea01bc04e8819bb734
Salonilanke-dev <saloni.cs23173@mmcc.edu.in>
2026-07-13 16:04:54 +0530

    Added Subtraction program

---
+++ subtraction.java
@@ -0,0 +1,11 @@
+public class Subtraction {
+    public static void main(String[] args) {
+
+        int a = 20;
+        int b = 10;
+
+        int result = a - b;
+
+        System.out.println("Subtraction = " + result);
+    }
+}
