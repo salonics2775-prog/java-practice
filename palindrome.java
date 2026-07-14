ecb1de10e4eb43af5d2dfc6182f2e94652d4ba27
Saloni-Lanke <saloni.cs23173@mmcc.edu.in>
2026-07-14 12:03:46 +0530

    Added Palindrome program

---
+++ palindrome.java
@@ -0,0 +1,20 @@
+public class Palindrome {
+    public static void main(String[] args) {
+
+        int number = 121;
+        int original = number;
+        int reverse = 0;
+
+        while (number != 0) {
+            int digit = number % 10;
+            reverse = reverse * 10 + digit;
+            number = number / 10;
+        }
+
+        if (original == reverse) {
+            System.out.println("Palindrome Number");
+        } else {
+            System.out.println("Not a Palindrome Number");
+        }
+    }
+}
