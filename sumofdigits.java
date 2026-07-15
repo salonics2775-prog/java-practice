56c7f82b12399b93bdcad7dbb36900cfb3cb6ea5
Saloni-Lanke <saloni.cs23173@mmcc.edu.in>
2026-07-15 23:20:59 +0530

    Added Sum of Digits program

---
+++ sumofdigits.java
@@ -0,0 +1,14 @@
+public class SumOfDigits {
+    public static void main(String[] args) {
+
+        int number = 1234;
+        int sum = 0;
+
+        while (number != 0) {
+            sum = sum + number % 10;
+            number = number / 10;
+        }
+
+        System.out.println("Sum = " + sum);
+    }
+}
