5a2a049b1fef67e946258188349808078151997a
Saloni-Lanke <saloni.cs23173@mmcc.edu.in>
2026-07-15 23:22:57 +0530

    Added Simple Calculator program

---
+++ simplecalculator.java
@@ -0,0 +1,12 @@
+public class SimpleCalculator {
+    public static void main(String[] args) {
+
+        int a = 20;
+        int b = 10;
+
+        System.out.println("Addition = " + (a + b));
+        System.out.println("Subtraction = " + (a - b));
+        System.out.println("Multiplication = " + (a * b));
+        System.out.println("Division = " + (a / b));
+    }
+}
