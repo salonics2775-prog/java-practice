1be945c7562134d7c8e13ec698d4ebdf47182634
Saloni-Lanke <saloni.cs23173@mmcc.edu.in>
2026-07-18 16:24:49 +0530

    Added String Compare program

---
+++ stringcompare.java
@@ -0,0 +1,15 @@
+public class StringCompare {
+    public static void main(String[] args) {
+
+        String a="Java";
+        String b="Java";
+
+        if(a.equals(b)){
+            System.out.println("Strings are Equal");
+        }
+        else{
+            System.out.println("Strings are Not Equal");
+        }
+
+    }
+}
