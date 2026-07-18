ec56c31c41f19ea6fa962203fe42ff26dd039932
Saloni-Lanke <saloni.cs23173@mmcc.edu.in>
2026-07-18 16:26:11 +0530

    Added Method Demo program

---
+++ methoddemo.java
@@ -0,0 +1,13 @@
+public class MethodDemo {
+
+    static void greeting(){
+        System.out.println("Welcome to Java");
+    }
+
+    public static void main(String[] args) {
+
+        greeting();
+
+    }
+
+}
