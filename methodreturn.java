1c942e78d75ec87c089dae473e67f43a4e1e9508
Saloni-Lanke <saloni.cs23173@mmcc.edu.in>
2026-07-19 15:21:41 +0530

    Added Method Return program

---
+++ methodreturn.java
@@ -0,0 +1,14 @@
+public class MethodReturn {
+
+    static int add(int a,int b){
+        return a+b;
+    }
+
+    public static void main(String[] args) {
+
+        int answer = add(20,30);
+
+        System.out.println(answer);
+
+    }
+}
