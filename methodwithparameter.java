9799b930f6924576389218a78e1f4f4f4a9edd9b
Saloni-Lanke <saloni.cs23173@mmcc.edu.in>
2026-07-19 15:21:14 +0530

    Added Method with Parameter program

---
+++ methodwithparameter.java
@@ -0,0 +1,12 @@
+public class MethodWithParameter {
+
+    static void greet(String name){
+        System.out.println("Welcome " + name);
+    }
+
+    public static void main(String[] args) {
+
+        greet("Saloni");
+
+    }
+}
