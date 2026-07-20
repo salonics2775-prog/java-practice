21b0a3f3bdef92d0dbb3ebc5aac140e3242aec5b
Saloni-Lanke <saloni.cs23173@mmcc.edu.in>
2026-07-20 23:12:41 +0530

    Added String Contains program

---
+++ stringcontains.java
@@ -0,0 +1,11 @@
+public class StringContains {
+
+    public static void main(String[] args) {
+
+        String text = "Java Programming";
+
+        System.out.println(text.contains("Java"));
+
+    }
+
+}
