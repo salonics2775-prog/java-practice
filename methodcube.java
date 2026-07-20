233361719d7486ee21ce9ea6a379b5f82be27b2b
Saloni-Lanke <saloni.cs23173@mmcc.edu.in>
2026-07-20 23:14:28 +0530

    Added Method Cube program

---
+++ methodcube.java
@@ -0,0 +1,15 @@
+public class MethodCube {
+
+    static int cube(int number) {
+        return number * number * number;
+    }
+
+    public static void main(String[] args) {
+
+        int answer = cube(3);
+
+        System.out.println(answer);
+
+    }
+
+}
