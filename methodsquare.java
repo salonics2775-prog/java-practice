71fcdfe761327733087283b850a8a1c3422f3821
Saloni-Lanke <saloni.cs23173@mmcc.edu.in>
2026-07-20 23:13:26 +0530

    Added Method Square program

---
+++ methodsquare.java
@@ -0,0 +1,15 @@
+public class MethodSquare {
+
+    static int square(int number) {
+        return number * number;
+    }
+
+    public static void main(String[] args) {
+
+        int answer = square(5);
+
+        System.out.println(answer);
+
+    }
+
+}
