da66efe9e88e7986204379a6ad349411ec2d432a
Saloni-Lanke <saloni.cs23173@mmcc.edu.in>
2026-07-21 15:06:28 +0530

    Added Replace program

---
+++ replace.java
@@ -0,0 +1,11 @@
+public class Replace {
+
+    public static void main(String[] args) {
+
+        String text = "I like Java";
+
+        System.out.println(text.replace("Java", "Python"));
+
+    }
+
+}
