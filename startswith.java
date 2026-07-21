4c96c90a48a992e24b7fd66d62997805cadda646
Saloni-Lanke <saloni.cs23173@mmcc.edu.in>
2026-07-21 15:07:04 +0530

    Added StartsWith program

---
+++ startswith.java
@@ -0,0 +1,11 @@
+public class StartsWith {
+
+    public static void main(String[] args) {
+
+        String text = "Java Programming";
+
+        System.out.println(text.startsWith("Java"));
+
+    }
+
+}
