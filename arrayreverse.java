2462d72e787aa4639426f1d75ef6a6830e7308f2
Saloni-Lanke <saloni.cs23173@mmcc.edu.in>
2026-07-22 20:10:21 +0530

    Added Array Reverse program

---
+++ arrayreverse.java
@@ -0,0 +1,15 @@
+public class ArrayReverse {
+
+    public static void main(String[] args) {
+
+        int[] numbers = {10, 20, 30, 40, 50};
+
+        for (int i = numbers.length - 1; i >= 0; i--) {
+
+            System.out.println(numbers[i]);
+
+        }
+
+    }
+
+}
