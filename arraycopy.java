9e5c4c112a31cee87d817f3890f2cea2bb6962f7
Saloni-Lanke <saloni.cs23173@mmcc.edu.in>
2026-07-23 18:18:19 +0530

    Added Array Copy program

---
+++ arraycopy.java
@@ -0,0 +1,22 @@
+public class ArrayCopy {
+
+    public static void main(String[] args) {
+
+        int[] first = {10, 20, 30, 40, 50};
+        int[] second = new int[first.length];
+
+        for (int i = 0; i < first.length; i++) {
+
+            second[i] = first[i];
+
+        }
+
+        for (int i = 0; i < second.length; i++) {
+
+            System.out.println(second[i]);
+
+        }
+
+    }
+
+}
