ab3ee491593890cc7a5bff83c4ac297daf75b505
Saloni-Lanke <saloni.cs23173@mmcc.edu.in>
2026-07-24 20:55:01 +0530

    Added Matrix Diagonal program

---
+++ matrixdiagonal.java
@@ -0,0 +1,18 @@
+public class MatrixDiagonal {
+
+    public static void main(String[] args) {
+
+        int[][] matrix = {
+                {1, 2},
+                {3, 4}
+        };
+
+        for (int i = 0; i < matrix.length; i++) {
+
+            System.out.println(matrix[i][i]);
+
+        }
+
+    }
+
+}
