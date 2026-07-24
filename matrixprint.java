5db1bbf23c4e4956b65398a25134fdcccc0f2e15
Saloni-Lanke <saloni.cs23173@mmcc.edu.in>
2026-07-24 20:50:52 +0530

    Added Matrix Print program

---
+++ matrixprint.java
@@ -0,0 +1,47 @@
+public class MatrixPrint {
+
+    public static void main(String[] args) {
+
+        int[][] matrix = {
+                {1, 2},
+                {3, 4}
+        };
+
+        for (int i = 0; i < matrix.length; i++) {
+
+            for (int j = 0; j < matrix[i].length; j++) {
+
+                System.out.print(matrix[i][j] + " ");
+
+            }
+
+            System.out.println();
+
+        }
+
+    }
+
+}public class MatrixPrint {
+
+    public static void main(String[] args) {
+
+        int[][] matrix = {
+                {1, 2},
+                {3, 4}
+        };
+
+        for (int i = 0; i < matrix.length; i++) {
+
+            for (int j = 0; j < matrix[i].length; j++) {
+
+                System.out.print(matrix[i][j] + " ");
+
+            }
+
+            System.out.println();
+
+        }
+
+    }
+
+}
